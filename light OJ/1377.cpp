5a73c39de4109863fae5a5816d1343f1d3b80f9a
Moontasir <moontasir042@gmail.com>
2021-01-11 12:51:04 +0600

    January 11, 2020

--- light OJ/1377.cpp
+++ light OJ/1377.cpp
@@ -1,124 +1,200 @@
-// # Problem Summary
 
-// * We are given a 2d maze and we have to reach from 'P' to 'D' using minimum move possible. 
-// * if we find '*' on the way. we have to continue form other '*'
-
-// # Solution Idea
-// Here we will mainly use `Priority Queue` to solve this problem. 
-// * we have to mark all portal ('*') in a vector list.  
-// * create data type (struct) to keep track mark and minimum distance.
-// * start from 'P'. when you get a portal ('*') with distance d, mark all other the portal d+2 and all other portal d+1. also push them in the queue. it will be wise if you keep track on the min distance for these portal. So that you dont have to count all of them agian and again .
-
-
+/***
+ *                                ╔╦╗╔═╗╔═╗╔╗╔╔╦╗╔═╗╔═╗╦╦═╗  ╔╦╗╔═╗╦ ╦╔╦╗╔═╗╔═╗╔╦╗ 
+ *                                ║║║║ ║║ ║║║║ ║ ╠═╣╚═╗║╠╦╝  ║║║╠═╣╠═╣║║║║ ║║ ║ ║║ 
+ *                                ╩ ╩╚═╝╚═╝╝╚╝ ╩ ╩ ╩╚═╝╩╩╚═  ╩ ╩╩ ╩╩ ╩╩ ╩╚═╝╚═╝═╩╝ 
+ *    ╦╔╗╔╔═╗╔═╗╦═╗╔╦╗╔═╗╔╦╗╦╔═╗╔╗╔  ╔═╗╔╗╔╔╦╗  ╔═╗╔═╗╔╦╗╔╦╗╦ ╦╔╗╔╦╔═╗╔═╗╔╦╗╦╔═╗╔╗╔  ╔═╗╔╗╔╔═╗╦╔╗╔╔═╗╔═╗╦═╗╦╔╗╔╔═╗ 
+ *    ║║║║╠╣ ║ ║╠╦╝║║║╠═╣ ║ ║║ ║║║║  ╠═╣║║║ ║║  ║  ║ ║║║║║║║║ ║║║║║║  ╠═╣ ║ ║║ ║║║║  ║╣ ║║║║ ╦║║║║║╣ ║╣ ╠╦╝║║║║║ ╦ 
+ *    ╩╝╚╝╚  ╚═╝╩╚═╩ ╩╩ ╩ ╩ ╩╚═╝╝╚╝  ╩ ╩╝╚╝═╩╝  ╚═╝╚═╝╩ ╩╩ ╩╚═╝╝╚╝╩╚═╝╩ ╩ ╩ ╩╚═╝╝╚╝  ╚═╝╝╚╝╚═╝╩╝╚╝╚═╝╚═╝╩╚═╩╝╚╝╚═╝ 
+ *                            ╦ ╦╔╗╔╦╦  ╦╔═╗╦═╗╔═╗╦╔╦╗╦ ╦  ╔═╗╔═╗  ╦═╗╔═╗ ╦╔═╗╦ ╦╔═╗╦ ╦╦ 
+ *                            ║ ║║║║║╚╗╔╝║╣ ╠╦╝╚═╗║ ║ ╚╦╝  ║ ║╠╣   ╠╦╝╠═╣ ║╚═╗╠═╣╠═╣╠═╣║ 
+ *                            ╚═╝╝╚╝╩ ╚╝ ╚═╝╩╚═╚═╝╩ ╩  ╩   ╚═╝╚    ╩╚═╩ ╩╚╝╚═╝╩ ╩╩ ╩╩ ╩╩ 
+ *                                          https://github.com/Munmud 
+ *                                           moontasir042@gmail.com 
+ */
 
 
 #include <bits/stdc++.h>
 
+#define setinf(ar)              memset(ar,126,sizeof ar)
+#define MEM(a, b)               memset ( a, (b), sizeof(a) )
+#define MAX(a, b)               ((a) > (b) ? (a) : (b))
+#define MIN(a, b)               ((a) < (b) ? (a) : (b))
+#define ABS(X)                  ( (X) > 0 ? (X) : ( -(X) ) )
+#define S(X)                    ( (X) * (X) )
+#define SZ(V)                   (int )V.size()
 #define FORN(i, n)              for(int i = 0; i < n; i++)
+#define FORAB(i, a, b)          for(int i = a; i <= b; i++)
+#define ALL(V)                  V.begin(), V.end()
+#define ALLR(V)                 V.rbegin(), V.rend()
+#define IN(A, B, C)             ((B) <= (A) && (A) <= (C))
+#define AIN(A, B, C)            assert(IN(A, B, C))
+
+#define wa2(x , y)              cout << (#x) << " " << (#y)<< " is " << (x) << " " << (y)<< endl
+#define wa(x)                   cout << (#x) << " is " << (x) << endl
+
+#define ll                      long long int
 #define xx                      first
 #define yy                      second
+#define pb(x)                   push_back(x)
+#define PI                      acos(-1.0)
+
 #define PII                     pair<int, int>
+#define PLL                     pair<long long int, long long int>
+#define VI                      vector <int>
+#define VL                      vector <long long int>
+
 #define BOUNDARY(i, j, r , c)   ((i >= 0 && i < r) && (j >= 0 && j < c))
+#define max3(x, y, z)           MAX(MAX(x, y), MAX(y, z))
+
+#define front_zero(n)           __builtin_clzll(n)
+#define back_zero(n)            __builtin_ctzll(n)
+#define total_one(n)            __builtin_popcountll(n)
+
 using namespace std;
 
-#define SIZE                (int)250
 
-/*----------------------Graph Moves----------------*/
-const int fx[]={+1,-1,+0,+0};
-const int fy[]={+0,+0,+1,-1};
-/*------------------------------------------------*/
+template < typename F, typename S >
+ostream& operator << ( ostream& os, const pair< F, S > & p ) {
+    return os << "(" << p.first << ", " << p.second << ")";
+}
+template <class T>
+ostream & operator << (ostream & os, vector <T> const& x) {
+    os << "{ ";
+    for(auto& y : x) os << y << " ";
+    return os << "}";
+}
+template <class T>
+ostream & operator << (ostream & os, set <T> const& x) {
+    os << "{ ";
+    for(auto& y : x) os << y << " ";
+    return os << "}";
+}
+template < typename T >
+ostream &operator << ( ostream & os, const multiset< T > &v ) {
+    os << "[";
+    typename multiset< T > :: const_iterator it;
+    for ( it = v.begin(); it != v.end(); it++ ) {
+        if( it != v.begin() ) os << ", ";
+        os << *it;
+    }
+    return os << "]";
+}
+template < typename F, typename S >
+ostream &operator << ( ostream & os, const map< F, S > &v ) {
+    os << "[";
+    typename map< F , S >::const_iterator it;
+    for( it = v.begin(); it != v.end(); it++ ) {
+        if( it != v.begin() ) os << ", ";
+        os << it -> first << " = " << it -> second ;
+    }
+    return os << "]";
+}
+/*---------------------------------- x ------------------------------------*/
 
-struct data {
-    int x, y, d ;
-    data(int _x , int _y , int _d)
+#define N                	(int)210
+#define MOD                1000000007
+
+vector <PII> q[3] ;
+
+struct inffo{
+    PII pt ;
+    int d ;
+    inffo() {}
+    inffo(PII _pt , int _d)
     {
-        x= _x ;
-        y = _y ;
+        pt = _pt ;
         d = _d ;
     }
-    data(){}
-};
-bool operator < (data a , data b)
+} ;
+
+const int fx[]={+1,-1,+0,+0};
+const int fy[]={+0,+0,+1,-1};
+
+string maze[N] ;
+vector <PII> star ;
+PII st , ed ;
+int dist[N][N] ;
+bitset <N> vis[N] ; 
+
+void _main_main()
 {
-    return a.d > b.d ;
-}
-
-string maze[SIZE] ;
-int n , m;
-int dist[SIZE][SIZE] ;
-
-
-
-int solve()
-{
-    priority_queue <data> q ;
-    vector <PII> v ;
-    data st ;
-    bitset <SIZE> vis[SIZE] ;
-    
+    star.clear() ;
+    int n,m ;
     cin >> n >> m ;
     FORN(i,n){
         cin >> maze[i] ;
-        vis[i].reset() ;
         FORN(j,m){
-            if (maze[i][j] == 'P' ) st.x = i , st.y = j ;
-            else if (maze[i][j] == '*') v.push_back({i,j}) ;
-            else if (maze[i][j] == '#') vis[i][j] = 1 ;
+            if (maze[i][j] == '*') star.push_back({i,j}) ;
+            else if (maze[i][j] == 'P') st = {i,j} ;
+            else if (maze[i][j] == 'D') ed = {i,j} ;
             dist[i][j] = INT_MAX ;
         }
     }
+    // wa2(st.xx,st.yy) ;
 
-    st.d = 0 ;
-    q.push(st) ;
-    dist[st.x][st.y] = 0 ;
-
-    while(!q.empty())
+    queue <inffo> q ;
+    q.push(inffo(st,0)) ;
+    dist[st.xx][st.yy] = 0 ;
+    bool maze_vis = 0 ;
+    int star_cnt = 0 ;
+    while (!q.empty())
     {
-        int x = q.top().x ;
-        int y = q.top().y ;
-        int d = q.top().d ;
+        
+        PII pt = q.front().pt ;
+        int d = q.front().d ;
+        // wa2(pt.xx,pt.yy) ;
+        // wa(d) ;
         q.pop() ;
+        
+        int val = dist[pt.xx][pt.yy] ;
 
-        if (dist[x][y] != d) continue ;
-        vis[x][y] = 1 ;
-        // cout << x << " " << y << " " << d << "\n" ;
+        if ( val!= d ) continue ;
 
-        if (maze[x][y] == 'D') return d ;
-
+        if (maze[pt.xx][pt.yy] == '*' &&  maze_vis == 0){
+            maze_vis = 1 ;
+            if (star.size()>1) {
+                for (auto i : star){
+                    if (i == pt) continue ;
+                    dist[i.xx][i.yy] = val+1 ;
+                    q.push(inffo(i, val+1)) ;
+                }
+                if (star_cnt == 1){
+                   dist[pt.xx][pt.yy] = val+2 ;
+                    q.push(inffo(pt, val+2)) ; 
+                }
+                else {
+                    dist[pt.xx][pt.yy] = val+1 ;
+                    q.push(inffo(pt, val+1)) ; 
+                }
+                
+            }
+            continue ;
+        }
 
         FORN(k,4)
         {
-            int xx = x + fx[k] ;
-            int yy = y + fy[k] ;
+            int x = pt.xx + fx[k] ;
+            int y = pt.yy + fy[k] ;
 
-            if (!BOUNDARY(xx,yy,n,m)) continue ;
-            if (vis[xx][yy]) continue ;
-            if (maze[xx][yy] == '*')
-            {
-                for (auto i : v)
-                {
-                    if (i == PII{xx,yy}) continue ;
-                    if (dist[i.xx][i.yy] > d+2){
-                    // wa2(i.xx,i.yy) ;
-                        dist[i.xx][i.yy] = d+2 ;    
-                        q.push(data(i.xx,i.yy,d+2)) ;
+            if ( !BOUNDARY(x,y,n,m) ) continue ;
+            if ( maze[x][y] =='#' ) continue ;
+            else{
 
-                    }
-                   if (dist[xx][yy] > d+3){
-                        dist[xx][yy] = d+3 ;
-                        q.push(data(xx,yy,d+3)) ;
-                    }
+                if (PII {x,y} == ed) {
+                    cout << val+1 << "\n" ;
+                    return ;
                 }
+                 if (dist[x][y] > val+1){
+                     if (maze[x][y] == '*')star_cnt++ ;
+                    dist[x][y] =  val+1 ;
+                    q.push(inffo({x,y} , dist[x][y])) ;
+                 }
             }
-            else if (dist[xx][yy] > d+1){
-                dist[xx][yy] = d+1 ;
-                q.push(data(xx,yy,d+1)) ;
-            }
-            
+
         }
     }
-    return INT_MAX ;
-
+    cout << "impossible\n" ;
 }
 
 
@@ -129,12 +205,10 @@ int main ()
     cin.tie(0);
     cout.tie(0);
 
-    int testCase = 1 ;cin >> testCase ;
+    int testCase = 1 ; cin >> testCase ;
     for (int i = 0; i < testCase; i++){
-        cout << "Case " << i+1 << ": "  ;
-        int res = solve() ;
-        if(res == INT_MAX) cout << "impossible\n" ;
-        else cout << res << "\n" ;
+        cout << "Case " << i+1 << ": " ;
+        _main_main() ;
     }
         
 }
